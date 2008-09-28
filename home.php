0b2775a9c5f09d0467a0414abd6d1ce1cf4dbb68
sebi <sebastian@ic3berg.de>
2008-09-28 18:05:24 +0200

    Updated h1 color with css class

--- home.php
+++ home.php
@@ -24,7 +24,7 @@
 
   <?php foreach ( $posts as $post ) { ?>
     <div class="post <?php echo $post->statusname; ?>" id="post-<?php echo $post->id; ?>">
-      <h1 class="dark-gray post-title"><a href="<?php echo $post->permalink; ?>" title="<?php echo $post->title; ?>"><?php echo $post->title_out; ?></a></h1>
+      <h1 class="cool-blue post-title"><a class="cool-blue" href="<?php echo $post->permalink; ?>" title="<?php echo $post->title; ?>"><?php echo $post->title_out; ?></a></h1>
       <div class="post-meta">Posted on <?php echo $post->pubdate->out('F j, Y'); ?> by <?php echo $post->author->displayname ?> | Tagged with: <?php echo $post->tags_out; ?></div>
       <?php echo $post->content_out; ?>
     </div>
