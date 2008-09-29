3f55682607786dc958ee9947e25b3b41ced47319
sebastianp <sebastian@ic3berg.de>
2008-09-29 14:50:45 +0200

    Added clearing div

--- entry.single.php
+++ entry.single.php
@@ -21,11 +21,11 @@
     </div>							
   </div>
   <!-- end sidebar -->
-
   <div class="post <?php echo $post->statusname; ?>" id="post-<?php echo $post->id; ?>">
     <h1 class="cool-blue post-title"><a class="cool-blue" href="<?php echo $post->permalink; ?>" title="<?php echo $post->title; ?>"><?php echo $post->title_out; ?></a></h1>
     <div class="post-meta">Posted on <?php echo $post->pubdate->out('F j, Y'); ?> by <?php echo $post->author->displayname ?> | Tagged with: <?php echo $post->tags_out; ?></div>
     <?php echo $post->content_out; ?>
   </div>
+  <div class="clear">&nbsp;</div>
 </div> <!-- end page --> 
 <?php $theme->display('footer'); ?>
