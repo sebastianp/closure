a1f8ec234e5495b9733ece099be3303799e5bb5f
sebastianp <sebastian@ic3berg.de>
2008-10-16 18:24:49 +0200

    Links displayed are fetched from an array defined in theme.php

--- display_links.php
+++ display_links.php
@@ -1,3 +1,13 @@
 <ul class="no-list-style">
-  <li><a href="http://twitter.com/sebastianp">Follow me on Twitter</a></li>
+  <?php
+    if($theme->links_list) {
+      foreach($theme->links_list as $link_text => $link) {
+        echo "<li><a href=\"".$link."\">".$link_text."</a></li>";
+      }
+    } else {
+      ?>
+      Please define your links list in theme.php.
+      <?php
+    }
+  ?>
 </ul>
