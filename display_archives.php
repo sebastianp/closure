9914c8594c45d7e7777a86c823e787f7d3841bf6
sebastianp <sebastian@ic3berg.de>
2008-10-16 18:24:25 +0200

    Added RN Archives plugin for monthly archive display

--- display_archives.php
+++ display_archives.php
@@ -1,2 +1,10 @@
 <?php
+if (Plugins::is_loaded('RN Monthly Archives')) {
+  // RN Monthly Archives Plugin required
+  $theme->monthly_archives($theme->rn_archives_months);
+} else {
+  ?>
+  Please install/activate the RN Archives Plugins. Thanx.
+  <?php
+}
 ?>
