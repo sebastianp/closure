952e090221791eda1b39b71dd7e339093651b156
Sebastian Pascu <sebastian@ic3berg.de>
2008-11-26 10:59:50 +0100

    Added comment marking end post div

--- entry.single.php
+++ entry.single.php
@@ -25,7 +25,7 @@
     <h1 class="cool-blue post-title"><a class="cool-blue" href="<?php echo $post->permalink; ?>" title="<?php echo $post->title; ?>"><?php echo $post->title_out; ?></a></h1>
     <div class="post-meta">Posted on <?php echo $post->pubdate->out('F j, Y'); ?> by <?php echo $post->author->displayname ?> | Tagged with: <?php echo $post->tags_out; ?></div>
     <?php echo $post->content_out; ?>
-  </div>
+  </div> <!-- end post -->
   <?php include 'comments.php' ?>
   <div class="clear">&nbsp;</div>
 </div> <!-- end page --> 
